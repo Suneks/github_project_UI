e9d141d0cce6600a9ac700cb86394b92ba1627a3
Suneks <sanews18@gmail.com>
2022-05-19 14:18:46 +0300

    added tests

--- src/test/java/config/application/AppConfig.java
+++ src/test/java/config/application/AppConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/application/app.properties"
+        "classpath:config/application/remote.properties"
 })
 public interface AppConfig extends Config {
 
